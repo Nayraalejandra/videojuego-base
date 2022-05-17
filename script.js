40300589e4a0491e7a775264b2f44b28cd481b0b
Nayra <arboledanayraalejandra@gmail.com>
2022-05-17 13:14:15 +0200

    conejos

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+
 let menuAbierto = false;
+let conejodie = false;
 
 
 function abrirMenu(){
     if(menuAbierto == false) {
-        interiorMenu.style.display="block"
+        $("#interiorMenu").css("display","block")
             menuAbierto =true
     }else{
-        interiorMenu.style.display="none"
+        $("#interiorMenu").css("display","none")
             menuAbierto = false;
     }
 }
 
 $("#menu").click(abrirMenu)
+
+function conejoMuerto(){
+    if(conejodie == false) {
+        $(".conejosImg").css("margin-top","-300px");
+        console.log("uyu");
+        conejodie = true;
+    }else{
+        $(".conejosImg").css("margin-top","00px")
+        console.log("eye")
+            conejodie = false;
+    }
+}
+
+$(".conejosImg").click(conejoMuerto)
