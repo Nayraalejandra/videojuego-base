fe828cb043caa0680ae09347ea9bc70bddcb09db
Nayraalejandra <arboledanayraalejandra@gmail.com>
2022-05-21 12:39:49 +0200

    inicio

--- JS/scriptInicio.js
+++ JS/scriptInicio.js
@@ -1,18 +1,39 @@
 
-let nombre = document.getElementById("askName");
-let juego =document.getElementById("pregunta")
+// let nombre = document.getElementById("askName");
+// let juego =document.getElementById("pregunta")
+
+// let vamos =document.getElementById("radio-1")
+
+// $("#boton").click(enviarfuncion)
 
 
-$("#boton").click(enviarfuncion)
+
+// function enviarfuncion(){
+//     if(nombre.value==""||nombre.value==null){
+//         alert("Escribe un nombre");
+//     }else if(juego.value=="" || juego.value==null){
+//         alert("Elige una opción")
+//     }else if (juego.value=="radio-1"){
+//         console.log("jfjbfksb")
+//     }
+// }
 
 
 
-function enviarfuncion(){
-    if(nombre.value==""||nombre.value==null){
-        alert("Escribe un nombre");
-    // }else if(juego.value=="" || juego.value==null){
-    //     alert("Elige una opción")
-    // }else{
-    //     console.log("jfjbfksb")
-    // }
-}
+$("#boton").click(function(){
+    // if($("#askName").value(null)){ 
+    //     alert("Escribe un nombre");
+
+     if($("#radio-1").prop("checked")== true && !$("#askName").val([])) {
+        window.location.href = "juego.html";
+       
+    }else if($("#radio-2").prop("checked")==true){
+        window.location.href = "index.html";
+       
+    } else if($("#pregunta").prop("checked")==null){
+        alert("Elige una opción")
+    }
+
+})
+
+
